ddee901ae103d3b3f632074187fce1708d80112a
Caylin Vu <caylinvu@gmail.com>
2023-10-12 17:39:13 -0500

    Set up skeleton controller functions

---
+++ controllers/gameController.js
@@ -0,0 +1,48 @@
+const Game = require("../models/game");
+const Category = require("../models/category");
+const asyncHandler = require("express-async-handler");
+
+// Display home page
+exports.index = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Site Home Page");
+});
+
+// Display list of all Games
+exports.game_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Game list");
+});
+
+// Display detail page for a specific Game
+exports.game_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Game detail: ${req.params.id}`);
+});
+
+// Display Game create form on GET
+exports.game_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book create GET");
+});
+
+// Handle Game create on POST
+exports.game_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book create POST");
+});
+
+// Display Game delete form on GET
+exports.game_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Game delete GET");
+});
+
+// Handle Game delete on POST
+exports.game_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Game delete POST");
+});
+
+// Display Game update form on GET
+exports.game_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Game update GET");
+});
+
+// Handle Game update on POST
+exports.game_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Game update POST");
+});

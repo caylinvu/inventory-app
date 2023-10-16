7bcf366f26748e5d6059f3127c86690784181036
Caylin Vu <caylinvu@gmail.com>
2023-10-16 12:47:07 -0500

    Set up get/post controller functions to add game

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -47,13 +47,83 @@ exports.game_detail = asyncHandler(async (req, res, next) => {
 
 // Display Game create form on GET
 exports.game_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create GET");
+  const allCategories = await Category.find({}, "name").exec();
+
+  res.render("game_form", {
+    title: "Add Game",
+    categories: allCategories,
+  });
 });
 
 // Handle Game create on POST
-exports.game_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create POST");
-});
+exports.game_create_post = [
+  (req, res, next) => {
+    if (!(req.body.category instanceof Array)) {
+      if (typeof req.body.category === "undefined") req.body.category = [];
+      else req.body.category = new Array(req.body.category);
+    }
+    next();
+  },
+
+  body("title", "Game title required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Description required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category.*")
+    .escape(),
+  body("category", "Must choose at least one category")
+    .isArray({ min: 1 }),
+  body("price")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Price required")
+    .isCurrency({ 
+      allow_negatives: false, 
+      require_decimal: true, 
+      digits_after_decimal: 2, 
+      require_symbol: false, 
+      symbol_after_digits: false 
+    })
+    .withMessage("Price must be a positive number and contain 2 digits after the decimal; eg: 17.99, 16.00, etc."),
+  body("quantity")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const game = new Game({
+      title: req.body.title,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      quantity: req.body.quantity,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find({}, "name").exec();
+
+      for (const category of allCategories) {
+        if (game.category.includes(category._id)) {
+          category.checked = "true";
+        }
+      }
+      res.render("game_form", {
+        title: "Add Game",
+        categories: allCategories,
+        game: game,
+        errors: errors.array(),
+      });
+    } else {
+      await game.save();
+      res.redirect(game.url);
+    }
+  }),
+];
 
 // Display Game delete form on GET
 exports.game_delete_get = asyncHandler(async (req, res, next) => {
